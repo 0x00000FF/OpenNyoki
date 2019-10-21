3342de525678dec6fb74ce84e0492728631a6e27
Kangjun Heo == P.Knowledge <knowledge@patche.me>
2019-10-21 20:48:27 +0900

    Event Process

--- Solutions/OpenNyoki/GameWindow.cs
+++ Solutions/OpenNyoki/GameWindow.cs
@@ -10,16 +10,23 @@ namespace OpenNyoki
     class GameWindow
     {
         private RenderWindow m_RenderWindow = null;
+        private GameWindowEvents m_GameEvents = null;
 
-        public GameWindow(RenderWindow _window = null)
+        private Game m_Game = null;
+
+        public GameWindow(RenderWindow _window = null, 
+                          GameWindowEvents _events = null, 
+                          Game _game = null)
         {
             m_RenderWindow = _window ??
                              new RenderWindow(
                                  mode: new VideoMode(800, 600),
                                  title: "OpenNyoki"
                                  );
+            m_GameEvents = _events ?? new GameWindowEvents();
+            m_Game = _game ?? new Game();
 
-            AssignEvents();
+            AssignEvents(m_GameEvents);
 
             while (m_RenderWindow.IsOpen)
             {
@@ -31,6 +38,14 @@ namespace OpenNyoki
         private void AssignEvents(GameWindowEvents events)
         {
             m_RenderWindow.Closed += new EventHandler(events.WindowClosed);
+            m_RenderWindow.GainedFocus += new EventHandler(events.WindowGainedFocus);
+            m_RenderWindow.KeyPressed += new EventHandler<KeyEventArgs>(events.KeyDown);
+            m_RenderWindow.KeyReleased += new EventHandler<KeyEventArgs>(events.KeyUp);
+            m_RenderWindow.LostFocus += new EventHandler(events.WindowLostFocus);
+            m_RenderWindow.MouseButtonPressed += new EventHandler<MouseButtonEventArgs>(events.MouseDown);
+            m_RenderWindow.MouseButtonReleased += new EventHandler<MouseButtonEventArgs>(events.MouseUp);
+            m_RenderWindow.MouseMoved += new EventHandler<MouseMoveEventArgs>(events.MouseMove);
+            m_RenderWindow.MouseWheelScrolled += new EventHandler<MouseWheelScrollEventArgs>(events.MouseScrolled);
         }
     }
 }
--- Solutions/OpenNyoki/GameWindowEvents.cs
+++ Solutions/OpenNyoki/GameWindowEvents.cs
@@ -1,10 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SFML.Window;
 
 namespace OpenNyoki
 {
     class GameWindowEvents
     {
+        public void WindowClosed(object sender, EventArgs e)
+        {
+
+        }
+
+        public void WindowGainedFocus(object sender, EventArgs e)
+        {
+
+        }
+
+        public void KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        public void KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        public void WindowLostFocus(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        public void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        public void MouseMove(object sender, MouseMoveEventArgs e)
+        {
+            
+        }
+
+        public void MouseScrolled(object sender, MouseWheelScrollEventArgs e)
+        {
+            
+        }
     }
 }
