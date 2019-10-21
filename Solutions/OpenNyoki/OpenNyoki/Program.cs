acc54223cab63451464c08dfbe14e2fe083a74d9
Kangjun Heo == P.Knowledge <knowledge@patche.me>
2019-10-21 17:02:37 +0900

    Project Setup

---
+++ Solutions/OpenNyoki/OpenNyoki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenNyoki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
