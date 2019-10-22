ec606d7a3cb1a7cb6265ea7064579fad11087438
Kangjun Heo == P.Knowledge <knowledge@patche.me>
2019-10-22 18:31:15 +0900

    Documentations

--- Solutions/OpenNyoki/Models/Nyoki.cs
+++ Solutions/OpenNyoki/Models/Nyoki.cs
@@ -4,25 +4,42 @@ using System.Text;
 
 namespace OpenNyoki.Models
 {
+    /// <summary>
+    /// Determines Panel that Nyoki will be shown.
+    /// </summary>
     public enum NyokiPanel
     {
         Left, Right
     }
 
+    /// <summary>
+    /// Determines Type of Nyoki.
+    /// </summary>
     public enum NyokiTypes
     {
         Green = 0, Red = 1, Blue = 2, Purple = 3, Yellow = 4
     }
 
+    /// <summary>
+    /// Determines Position of Nyoki.
+    /// </summary>
     public struct NyokiPosition
     {
         public NyokiPanel Panel { get; set; } 
         public byte X { get; set; }
         public byte Y { get; set; }
 
+        /// <summary>
+        /// Creates position structure with position (0, 0) and user defined panel info
+        /// </summary>
+        /// <param name="panel">Desired panel to show nyoki</param>
+        /// <returns>A NyokiPosition structure</returns>
         public static NyokiPosition CreatePosition(NyokiPanel panel) => new NyokiPosition { Panel = panel };
     }
 
+    /// <summary>
+    /// Describes a Nyoki
+    /// </summary>
     public class Nyoki : GameObject
     {
         public bool IsPinNyoki { get; set; } = false;
