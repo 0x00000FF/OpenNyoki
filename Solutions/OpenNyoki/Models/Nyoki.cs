397ca22677bbb5881cd87b13deec456fa3f3527a
Kangjun Heo == P.Knowledge <knowledge@patche.me>
2019-10-22 18:02:13 +0900

    Nyoki Properties

--- Solutions/OpenNyoki/Models/Nyoki.cs
+++ Solutions/OpenNyoki/Models/Nyoki.cs
@@ -4,12 +4,35 @@ using System.Text;
 
 namespace OpenNyoki.Models
 {
+    public enum NyokiPanel
+    {
+        Left, Right
+    }
+
     public enum NyokiTypes
     {
+        Green = 0, Red = 1, Blue = 2, Purple = 3, Yellow = 4
+    }
 
+    public struct NyokiPosition
+    {
+        public NyokiPanel Panel { get; set; } 
+        public byte X { get; set; }
+        public byte Y { get; set; }
+
+        public static NyokiPosition CreatePosition(NyokiPanel panel) => new NyokiPosition { Panel = panel };
     }
 
     public class Nyoki : GameObject
     {
+        public bool IsPinNyoki { get; set; } = false;
+        public NyokiTypes Type { get; set; }
+        public NyokiPosition Position { get; set; }
+
+        public Nyoki(NyokiTypes type, NyokiPanel panel)
+        {
+            Type = type;
+            Position = NyokiPosition.CreatePosition(panel);
+        }
     }
 }
