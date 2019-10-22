0398fd45bd4d69b8fac98a8fc5ae9a65f1660b1e
Kangjun Heo == P.Knowledge <knowledge@patche.me>
2019-10-22 18:02:03 +0900

    Random Helper

---
+++ Solutions/OpenNyoki/Helper/RandomHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OpenNyoki.Helper
+{
+    public static class RandomHelper
+    {
+        public static int GetInt(int rangeMin, int rangeMax = -1)
+        {
+            // TODO: Implement Integer Random
+            throw new NotImplementedException();
+        }
+
+    }
+}
